362f59a0c40e9e7ebcce61872df348053d8c5db5
Yairys <yayi.arist.hz@gmail.com>
2022-10-16 21:51:22 -0300

    cambios en Index back

--- api/index.js
+++ api/index.js
@@ -4,23 +4,50 @@ const cors = require ('cors');
 const express =require('express');
 const bodyParser =require('body-parser');
 const routes = require('./src/routes/index');
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 const port = process.env.PORT || 3001
 
 const app= express()
 
 
 require('./src/db');
-app.use(express.json());
+
+app.options("*", cors());
+app.name = "API";
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next()
+});
+
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 app.use('/api', routes);
 
 app.use('/', express.static(path.join(__dirname, '../client/public')))
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.log(err);
+  res.status(status).send(message);
+});
 
 
 
 app.listen(port, ()=>{
   console.log(`*****listening on port: ${port}*******`)
 })
+
+module.exports = app;
