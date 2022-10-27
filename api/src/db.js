83d745a11583ca4a0799faeaa86cca360059c1a7
Yairys <yayi.arist.hz@gmail.com>
2022-10-27 16:38:49 -0300

    cambios en configuracion db

--- api/src/db.js
+++ api/src/db.js
@@ -1,16 +1,37 @@
 require("dotenv").config();
 const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 const ProductModel = require('./models/Product')
 const ProductCategory = require('./models/category.js')
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST 
+} = process.env;
 
 const sequelize = new Sequelize(`${DB_NAME}`, `${DB_USER}`, `${DB_PASSWORD}`, {
   host: `${DB_HOST}`,
   dialect: "mysql",
 });
 
-const Product = ProductModel(sequelize, Sequelize);
-const Category = ProductCategory(sequelize, Sequelize);
+const basename = path.basename(__filename);
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+  // Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach(model => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+const { Product, Category} = sequelize.models;
+
+/* const Product = ProductModel(sequelize, Sequelize);
+const Category = ProductCategory(sequelize, Sequelize); */
 
 Product.belongsTo(Category, { as: "categoryName", foreignKey: "category" });
 Category.hasMany(Product, { as: "product", foreignKey: "category" });
@@ -28,8 +49,7 @@ console.log(err)
 
 
 module.exports = {
-  Product,
-  Category     
+  ...sequelize.models
 };
 
 
